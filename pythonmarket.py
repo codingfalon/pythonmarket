39dca756684ef9e41678ed9e56d1cc8ef09050f6
Falon Shackelford <falon.shackelford@gmail.com>
2015-11-03 15:57:58 -0500

    added comments to code

--- pythonmarket.py
+++ pythonmarket.py
@@ -1,23 +1,23 @@
-shopping_list = ["banana", "orange", "apple"]
+shopping_list = ["banana", "orange", "apple"]  #creates a list of items
 
-stock = {
+stock = {                       #key/value pairs track items/quantities
     "banana": 6,
     "apple": 0,
     "orange": 32,
     "pear": 15
 }
     
-prices = {
+prices = {                      #key/value pairs track items/prices          
     "banana": 4,
     "apple": 2,
     "orange": 1.5,
     "pear": 3
 }
 
-def compute_bill(food):
+def compute_bill(food):         #creates function to calculate bill       
     total = 0
-    for item in food:
+    for item in food:           #iterates through list, adding price of item based on item amount in each iteration
         if stock[item] > 0:
             total += prices[item]
             stock[item] -= 1 
-    return total
+    return total                #calls the function, "total" represents the bill 
